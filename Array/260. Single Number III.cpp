70bfa2070a05e09ef0989c1ae75ff77ed524672b
Nikhil Kumar <108209923+Nikhil743@users.noreply.github.com>
2023-07-31 16:17:14 +0530

    Array

---
+++ Array/260. Single Number III.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int>m;
+        vector<int>ans;
+       for(int i = 0; i <nums.size(); i++){
+            m[nums[i]]++;
+        }
+        for(auto a : m){
+            if(a.second <= 1)ans.push_back(a.first);
+        }
+        return ans;
+    }
+};
